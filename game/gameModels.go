d9f4e98c84121f879a17876d67ccdc9eee4d60b2
dkasabovn <dkasabovn@gmail.com>
2021-04-19 19:08:39 -0500

    basic submissions

--- game/gameModels.go
+++ game/gameModels.go
@@ -12,14 +12,15 @@ const (
 
 type Question interface {
 	questionType() int
+	validate(userResponse interface{}) bool
 }
 
 type MultipleChoice struct {
-	QType     int      `firestore:"qType"`
-	Answer    int      `firestore:"answer"`
-	SQuestion string   `firestore:"question"`
-	Qid       int      `firestore:"qid"`
-	Choices   []string `firestore:"choices"`
+	QType     int      `firestore:"qType" json:"qType"`
+	Answer    int      `firestore:"answer" json:"answer"`
+	SQuestion string   `firestore:"question" json:"question"`
+	Qid       int      `firestore:"qid" json:"qid"`
+	Choices   []string `firestore:"choices" json:"choices"`
 }
 
 type FreeResponse struct {
@@ -28,12 +29,18 @@ type FreeResponse struct {
 	question string
 }
 
-func (fr *FreeResponse) validate(userResponse string) bool {
-	return fr.answer.Match([]byte(userResponse))
+func (fr *FreeResponse) validate(userResponse interface{}) bool {
+	if val, ok := userResponse.(string); ok {
+		return fr.answer.Match([]byte(val))
+	}
+	return false
 }
 
-func (mp *MultipleChoice) validate(userResponse int) bool {
-	return userResponse == mp.Answer
+func (mp *MultipleChoice) validate(userResponse interface{}) bool {
+	if val, ok := userResponse.(int); ok {
+		return val == mp.Answer
+	}
+	return false
 }
 
 func (fr *FreeResponse) questionType() int {
